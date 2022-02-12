02e71cc72b8e520199af67989809b64012531d4a
laksh <lakshyam38@gmail.com>
2022-02-12 12:15:33 +0530

    nav reformat

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,29 @@
 import React from "react";
+import { BsRecordCircle } from "react-icons/bs";
 
 const Nav = () => {
   return (
     <div className="nav-container">
-      <div className="nav-brand">GUARDIAN</div>
+      <div className="nav-brand">
+        <BsRecordCircle
+          size="1.6rem"
+          color="white"
+          style={{ marginRight: "8px" }}
+        />
+        GUARDIAN
+      </div>
       <div className="nav-links">
         <li className="nav-item">
           <a href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a href="/login">Login</a>
+          <a href="/">Family Tracker</a>
         </li>
         <li className="nav-item">
-          <a href="/signup">Signup</a>
+          <a href="/">My Location</a>
+        </li>
+        <li className="nav-item">
+          <a href="/">Profile</a>
         </li>
       </div>
     </div>
