bb4ad689e91953070ac4b303743c874875e5842b
laksh <lakshyam38@gmail.com>
2022-02-12 12:14:06 +0530

    layout basic

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AlertList from "./AlertList";
+import ButtonList from "./ButtonList";
+
+const Main = () => {
+  return (
+    <main>
+      <AlertList />
+      <ButtonList />
+    </main>
+  );
+};
+
+export default Main;
