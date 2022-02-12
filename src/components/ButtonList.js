45ecfe7d4ba2e494b3396858b1c12056eb2e7214
laksh <lakshyam38@gmail.com>
2022-02-12 12:14:56 +0530

    buttons

---
+++ src/components/ButtonList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ButtonList = () => {
+  return (
+    <div className="container">
+      <button className="help-btn">I need help!</button>
+      <button className="burg-btn">Burgular alert!</button>
+    </div>
+  );
+};
+
+export default ButtonList;
